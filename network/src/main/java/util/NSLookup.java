e016f56d1f700360be7ceb4d8cf557a3ee5a8be0
yuming0315 <gjdbal111@naver.com>
2022-12-28 18:01:16 +0900

    과제

--- network/src/main/java/util/NSLookup.java
+++ network/src/main/java/util/NSLookup.java
@@ -2,14 +2,27 @@ package util;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class NSLookup {
 
 	public static void main(String[] args) {
+		
 		try {
-			InetAddress[] inetAddresses = InetAddress.getAllByName("www.douzone.com");
-			
-			//구현하기 과제
+			while(true) {
+				Scanner sc = new Scanner(System.in);
+				System.out.print(">");
+				
+				String s = sc.nextLine();
+				if("exit".equals(s)) {
+					break;
+				}
+				InetAddress[] inetAddresses = InetAddress.getAllByName(s);
+				
+				for (InetAddress inetAddress : inetAddresses) {
+					System.out.println(inetAddress.getHostName()+" : "+inetAddress.getHostAddress());
+				}
+			}
 			
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
