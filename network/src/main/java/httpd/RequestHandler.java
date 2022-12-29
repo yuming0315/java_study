a69e7e43830f549ba189072dcf9af73fb545f78b
yuming0315 <gjdbal111@naver.com>
2022-12-29 15:53:30 +0900

    20221229 assignment

--- network/src/main/java/httpd/RequestHandler.java
+++ network/src/main/java/httpd/RequestHandler.java
@@ -12,92 +12,114 @@ import java.nio.file.Files;
 public class RequestHandler extends Thread {
 	private static final String DOCUMENT_ROOT = "./webapp";
 	private Socket socket;
-	
-	public RequestHandler( Socket socket ) {
+
+	public RequestHandler(Socket socket) {
 		this.socket = socket;
 	}
-	
+
 	@Override
 	public void run() {
 		try {
 			// get IOStream
 			OutputStream outputStream = socket.getOutputStream();
-			//파이프로 안 한 이유: 이미지도보내고 이것저것 보내야해서
-			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(),"utf-8"));
-			
+			// 파이프로 안 한 이유: 이미지도보내고 이것저것 보내야해서
+			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "utf-8"));
+
 			// logging Remote Host IP Address & Port
-			InetSocketAddress inetSocketAddress = ( InetSocketAddress )socket.getRemoteSocketAddress();
-			consoleLog( "connected from " + inetSocketAddress.getAddress().getHostAddress() + ":" + inetSocketAddress.getPort() );
-			String request=null;
-			while(true) {
-				String line =br.readLine();
-				//브라우저 연결을 끊으면 
-				if(line==null) {
+			InetSocketAddress inetSocketAddress = (InetSocketAddress) socket.getRemoteSocketAddress();
+			consoleLog("connected from " + inetSocketAddress.getAddress().getHostAddress() + ":"
+					+ inetSocketAddress.getPort());
+			String request = null;
+			while (true) {
+				String line = br.readLine();
+				// 브라우저 연결을 끊으면
+				if (line == null) {
 					break;
 				}
-				//simpleHttpServer는 요청의 헤더만 처리한다.
-				if("".equals(line)) {
+				// simpleHttpServer는 요청의 헤더만 처리한다.
+				if ("".equals(line)) {
 					break;
 				}
-				
-				//요청 헤더의 첫번째 라인만 읽음
-				if(request == null) {
-					request=line;
+
+				// 요청 헤더의 첫번째 라인만 읽음
+				if (request == null) {
+					request = line;
 					break;
 				}
 			}
-			
+
 //			consoleLog(request);
-			
+
 			String[] tokens = request.split(" ");
-			if("GET".equals(tokens[0])) {
+			if ("GET".equals(tokens[0])) {
 				consoleLog(request);
-				reponseStaticResource(outputStream,tokens[1],tokens[2]);
-			}else {
-				//methods: post,put,delete, head, connect
-				//simpleHttpServer에서는 무시할거다 모든통신은 답장?를 넘겨줘야함 (400bad resquest)
-				//reponse400Error(outputStream,tokens[2]); 구현하기 과제
+				reponseStaticResource(outputStream, tokens[1], tokens[2]);
+			} else {
+				// methods: post,put,delete, head, connect
+				// simpleHttpServer에서는 무시할거다 모든통신은 답장?를 넘겨줘야함 (400bad resquest)
+				reponse400Error(outputStream, tokens[2]); // 구현하기 과제
 			}
-			
-			
+
 			// 예제 응답입니다.
 			// 서버 시작과 테스트를 마친 후, 주석 처리 합니다.
 
-		} catch( Exception ex ) {
-			consoleLog( "error:" + ex );
+		} catch (Exception ex) {
+			consoleLog("error:" + ex);
 		} finally {
 			// clean-up
-			try{
-				if( socket != null && socket.isClosed() == false ) {
+			try {
+				if (socket != null && socket.isClosed() == false) {
 					socket.close();
 				}
-				
-			} catch( IOException ex ) {
-				consoleLog( "error:" + ex );
+
+			} catch (IOException ex) {
+				consoleLog("error:" + ex);
 			}
-		}			
+		}
 	}
 
-	private void reponseStaticResource (OutputStream outputStream, String url, String protocol) throws IOException{
-		//default(welcome) file set
-		if("/".equals(url)) {
-			url="/index.html";
+	private void reponse400Error(OutputStream outputStream, String protocol) throws IOException {
+		// HTTP/1.1 404 bad request
+		// Content-Type:....
+		// \r\n
+		// .....
+		request(outputStream,"/error/400.html",protocol);
+	}
+
+	private void reponseStaticResource(OutputStream outputStream, String url, String protocol) throws IOException {
+		// default(welcome) file set
+		if ("/".equals(url)) {
+			url = "/index.html";
 		}
-		
-		File file = new File(DOCUMENT_ROOT+url);
-		//nio
+		request(outputStream,url,protocol);
+	}
+
+	private void request(OutputStream outputStream, String url, String protocol) throws IOException {
+		File file = new File(DOCUMENT_ROOT + url);
+		if (!file.exists()) {
+			reponse404Error(outputStream, protocol);
+			return;
+		}
+		// nio
 		byte[] body = Files.readAllBytes(file.toPath());
 		String contentType = Files.probeContentType(file.toPath());
-		
-		//응답
-		outputStream.write( "HTTP/1.1 200 OK\r\n".getBytes( "UTF-8" ) );
-		outputStream.write( ("Content-Type:"+contentType+"; charset=utf-8\r\n").getBytes( "UTF-8" ) );
-		outputStream.write( "\r\n".getBytes() );
-		outputStream.write( body );
 
+		// 응답
+		outputStream.write((protocol + " 200 OK\r\n").getBytes("UTF-8"));
+		outputStream.write(("Content-Type:" + contentType + "; charset=utf-8\r\n").getBytes("UTF-8"));
+		outputStream.write("\r\n".getBytes());
+		outputStream.write(body);
 	}
 
-	public void consoleLog( String message ) {
-		System.out.println( "[httpd#" + getId() + "] " + message );
+	private void reponse404Error(OutputStream outputStream, String protocol) throws IOException {
+		// HTTP/1.1 404 Not Found
+		// Content-Type:....
+		// \r\n
+		// .....
+		request(outputStream,"/error/404.html",protocol);
+	}
+
+	public void consoleLog(String message) {
+		System.out.println("[httpd#" + getId() + "] " + message);
 	}
 }
